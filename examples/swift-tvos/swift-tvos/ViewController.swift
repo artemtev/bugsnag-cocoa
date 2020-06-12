591ec9c19a7cde55b78cf47d60be222a0a35f7ec
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-06-12 21:13:12 +0100

    Basic tvOS workspace with Bugsnag Pod included

---
+++ examples/swift-tvos/swift-tvos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-tvos
+//
+//  Created by Steve Kirkland on 12/06/2020.
+//  Copyright © 2020 Bugsnag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
