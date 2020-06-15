d3b053ea3b5a1fc0bd48a9c9097300571bcb6034
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-06-15 22:44:45 +0100

    Stack overflow and OOM added

--- examples/swift-tvos/swift-tvos/ViewController.swift
+++ examples/swift-tvos/swift-tvos/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textView.text = ""
     }
+
+    @IBOutlet weak var textView: UITextView!
 
     /*
      * Handled errors
@@ -29,7 +32,7 @@ class ViewController: UIViewController {
             }
         }
     }
-
+    
     @IBAction func notifyError(_ sender: Any) {
         do {
             throw NSError(domain: "com.bugsnag.example", code: 101, userInfo: [NSLocalizedDescriptionKey: "A handled error"])
@@ -53,7 +56,25 @@ class ViewController: UIViewController {
     }
     
     @IBAction func stackOverflow(_ sender: Any) {
+        let items = ["I'll overflow!"]
+        if sender is ViewController || sender is UIButton {
+            stackOverflow(self)
+        }
+        print("items: %@", items)
     }
+    
+    @IBAction func oom(_ sender: Any) {
+        let text = "var b = document.createElement('div'); div.innerHTML = 'Hello item %d'; document.documentElement.appendChild(div);\n";
+        for i in 0 ... (3000 * 1024) {
+            textView.text += text;
+            textView.setNeedsLayout()
+            if (i % 500 == 0) {
+                NSLog("Loaded %d items", i);
+            }
+        }
+
+    }
+    
     
 }
 
